c812bf05090751c84cc56efa0526d94059fc6181
gitThi254 <138993372+gitThi254@users.noreply.github.com>
2024-02-07 07:14:36 +0700

    update vite

--- src/components/ListBlogs.tsx
+++ src/components/ListBlogs.tsx
@@ -9,13 +9,13 @@ const ListBlogs = () => {
   if (error) return <p>{error.message}</p>;
   return (
     <div className="grid grid-cols-3 gap-10">
-      {blogs?.pages[0].map((item: any) => (
+      {blogs?.pages[0]?.map((item: any) => (
         <div key={item._id} className="">
           <div>{item.title}</div>
           <div>{item.discription}</div>
           <div>{item.category.category_name}</div>
           <div className="w-[200px] h-[200px] border border-1 border-slate-700 flex items-center justify-center">
-            <img src={item.images[0].url} alt="blog" />
+            <img src={item?.images[0]?.url} alt="blog" />
           </div>
           <div>
             <Link to={`/blog/${item._id}`}>read more</Link>
